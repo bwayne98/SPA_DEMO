085bb44a37840e65681cb96b274db98d5b385748
bwayne98 <bm.stamina@gmail.com>
2022-03-08 16:00:35 +0800

    seeder 優化 教室不重複使用

--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -14,13 +14,12 @@ class LessonFactory extends Factory
      */
     public function definition()
     {
-        $rooms = ["2A","2S","2B", "3A", "3S", "3B", "4A", "4S", "4B"];
+  
         $levels = ["Y1","Y2","Y3","Y4","R5","R6","R7","R8","R9","R10"];
         $codes = ["X","Y","Z"];
 
         return [
             'name' => Arr::random($levels).Arr::random($codes),
-            'room' => Arr::random($rooms)
         ];
     }
 }
